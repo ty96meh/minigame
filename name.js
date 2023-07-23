8629673e141e84b02eff8ffdc7425d3a13a46620
ty96meh <hetalianoneko@gmail.com>
2023-07-23 17:04:23 +0100

    Update

--- name.js
+++
@@ -1,6 +0,0 @@
-
-function submit(){
-    var currentName = document.getElementById("Username").value;
-    console.log(currentName);
-    location.href = 'ConfirmName.html?Username='+ currentName;
-}
